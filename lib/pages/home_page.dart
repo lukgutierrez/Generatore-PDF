ba516e7f919de997a0948a15c2a55a3f9c9cb3e7
Luk Gutierrez <lukgutierrezagustin@gmail.com>
2022-07-22 16:33:37 -0300

    Proyect Funtion

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,10 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:pdf/pdf.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:pdf/widgets.dart' as pw;
+import 'package:pdf_viewer_plugin/pdf_viewer_plugin.dart';
+import 'package:share_plus/share_plus.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -11,18 +12,21 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  pdfCreation() async {
+  generatePdf() async {
     final pdf = pw.Document();
+
     pdf.addPage(pw.Page(
         pageFormat: PdfPageFormat.a4,
         build: (pw.Context context) {
-          return pw.Center(
-            child: pw.Text("Hello Lenke"),
-          ); // Center
-        }));
-    Directory output = await getApplicationDocumentsDirectory();
-    final file = File("${output.path}/ejemplo1.pdf");
-    file.writeAsBytes(await pdf.save());
+          return pw.Center(child: pw.Text("Hello Fernanda")); // Center
+        })); // Page
+
+    Directory tempDir = await getApplicationDocumentsDirectory();
+    String tempPath = '${tempDir.path}/PaynmentMarket.pdf';
+    final file = File(tempPath);
+    var path = await file.writeAsBytes(await pdf.save());
+
+    return path.path;
   }
 
   @override
@@ -33,12 +37,44 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Center(
         child: ElevatedButton(
-            onPressed: () {
-              pdfCreation();
+            onPressed: () async {
+              var path = await generatePdf();
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => PdfViewer(
+                    path: path,
+                  ),
+                ),
+              );
             },
             child: Container(
               child: Text("Pdf Creator"),
             )),
+      ),
+    );
+  }
+}
+
+class PdfViewer extends StatelessWidget {
+  const PdfViewer({Key? key, this.path}) : super(key: key);
+  final path;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('PDF Viewer'),
+        actions: [
+          IconButton(
+              onPressed: () {
+                // ShareExtend.share(path, 'file');
+                Share.shareFiles([path]);
+              },
+              icon: Icon(Icons.share))
+        ],
+      ),
+      body: Container(
+        child: PdfView(path: path),
       ),
     );
   }
