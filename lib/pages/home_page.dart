4f69d41d19b5956e7fc845b609bbf980de6b0618
Luk Gutierrez <lukgutierrezagustin@gmail.com>
2022-07-25 16:34:06 -0300

    editpdf3

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -39,14 +39,14 @@ class _HomePageState extends State<HomePage> {
                   //DATE "1"
 
                   pw.SizedBox(
-                    height: 350.0,
+                    height: 300.0,
                     child: pw.Column(
                         mainAxisAlignment: pw.MainAxisAlignment.spaceBetween,
                         crossAxisAlignment: pw.CrossAxisAlignment.start,
                         children: [
                           pw.SizedBox(
-                            width: 250.0,
-                            height: 250.0,
+                            width: 200.0,
+                            height: 200.0,
                             child: pw.Image(
                               pw.MemoryImage(image),
                             ),
@@ -63,17 +63,17 @@ class _HomePageState extends State<HomePage> {
                           ),
                         ]),
                   ),
-
                   pw.Divider(height: 100.0),
                   //DATE "2"
                   pw.SizedBox(
-                    width: 400.0,
+                    width: 200.0,
                     child: pw.Column(
-                        mainAxisAlignment: pw.MainAxisAlignment.spaceBetween,
                         crossAxisAlignment: pw.CrossAxisAlignment.start,
                         children: [
-                          pw.Text("Trasferencia"),
-                          pw.Text("2000", style: pw.TextStyle(fontSize: 30.0))
+                          pw.Text("Trasferencia",
+                              style: pw.TextStyle(fontSize: 18.0, font: font)),
+                          pw.Text("S 2.000",
+                              style: pw.TextStyle(fontSize: 40.0, font: font))
                         ]),
                   ),
                   pw.Divider(height: 100.0),
@@ -85,50 +85,110 @@ class _HomePageState extends State<HomePage> {
                       children: [
                         //IMAGEN PUNTOS
                         pw.SizedBox(
-                          width: 100.0,
-                          height: 300.0,
+                          height: 200.0,
                           child: pw.Image(
                             pw.MemoryImage(image1),
                           ),
                         ),
                         //DUEÑO DE LA CUENTA DE MERCADO PAGO
-                        pw.Column(
-                            crossAxisAlignment: pw.CrossAxisAlignment.start,
-                            children: [
-                              pw.Text("De"),
-                              pw.Text("Juan Perez Almeida"),
-                              pw.Row(children: [
-                                pw.Text("CVU:"),
-                                pw.Text(
-                                    "0000483024932493849239429384028394394239480"),
-                              ]),
-                              pw.Text("Cuenta de Mercado Pago"),
-                              //PAGO A LA OTRA CUENTA
+                        pw.SizedBox(
+                          height: 250.0,
+                          child: pw.Column(
+                              mainAxisAlignment:
+                                  pw.MainAxisAlignment.spaceBetween,
+                              crossAxisAlignment: pw.CrossAxisAlignment.start,
+                              children: [
+                                //CUENTA MERCADOPAGO
+                                pw.Column(children: [
+                                  pw.Text("De",
+                                      style: pw.TextStyle(
+                                          fontSize: 18.0, font: font)),
+                                  pw.Text("Juan Perez Almeida",
+                                      style: pw.TextStyle(
+                                          fontSize: 30.0, font: font)),
+                                  pw.Row(children: [
+                                    pw.Text("CVU:",
+                                        style: pw.TextStyle(
+                                            fontSize: 18.0, font: font)),
+                                    pw.Text("000048302493249384",
+                                        style: pw.TextStyle(
+                                            fontSize: 18.0, font: font)),
+                                  ]),
+                                  pw.Text("Cuenta de Mercado Pago",
+                                      style: pw.TextStyle(
+                                          fontSize: 18.0, font: font)),
+                                ]),
 
-                              pw.Text("Para"),
-                              pw.Text("Roberto Copa Manuel"),
-                              pw.Row(children: [
-                                pw.Text("CUIT/CUIL"),
-                                pw.Text("20-32485893-5"),
+                                //PAGO A LA OTRA CUENTA
+                                pw.Column(
+                                    mainAxisAlignment:
+                                        pw.MainAxisAlignment.spaceBetween,
+                                    crossAxisAlignment:
+                                        pw.CrossAxisAlignment.start,
+                                    children: [
+                                      pw.Text("Para",
+                                          style: pw.TextStyle(
+                                              fontSize: 18.0, font: font)),
+                                      pw.Text("Roberto Copa Manuel",
+                                          style: pw.TextStyle(
+                                              fontSize: 30.0, font: font)),
+                                      pw.Row(children: [
+                                        pw.Text("CUIT/CUIL",
+                                            style: pw.TextStyle(
+                                                fontSize: 18.0, font: font)),
+                                        pw.Text("20-32485893-5",
+                                            style: pw.TextStyle(
+                                                fontSize: 18.0, font: font)),
+                                      ]),
+                                      pw.Text("Caja de ahorro",
+                                          style: pw.TextStyle(
+                                              fontSize: 18.0, font: font)),
+                                    ])
                               ]),
-                              pw.Text("Caja de ahorro"),
-                            ]),
+                        ),
                       ]),
                   //PAGO A LA OTRA CUENTA
 
                   pw.Divider(height: 100.0),
                   //DATE "4"
-                  pw.Column(
-                      mainAxisAlignment: pw.MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: pw.CrossAxisAlignment.start,
-                      children: [
-                        pw.Text("COELSA ID"),
-                        pw.Text("1LREYREUHBUEDGUYGDUE742386843286748"),
-                      ]),
-                  pw.Column(children: [
-                    pw.Text("Código de trasferencia"),
-                    pw.Text("24352653625365532"),
-                  ])
+                  pw.SizedBox(
+                    height: 140.0,
+                    child: pw.Column(
+                        crossAxisAlignment: pw.CrossAxisAlignment.start,
+                        mainAxisAlignment: pw.MainAxisAlignment.spaceBetween,
+                        children: [
+                          pw.SizedBox(
+                            height: 60.0,
+                            child: pw.Column(
+                                mainAxisAlignment:
+                                    pw.MainAxisAlignment.spaceBetween,
+                                crossAxisAlignment: pw.CrossAxisAlignment.start,
+                                children: [
+                                  pw.Text("COELSA ID",
+                                      style: pw.TextStyle(
+                                          fontSize: 18.0, font: font)),
+                                  pw.Text("1LREYREUHBUEDGUYGDUE742386843286748",
+                                      style: pw.TextStyle(
+                                          fontSize: 18.0, font: font)),
+                                ]),
+                          ),
+                          pw.SizedBox(
+                            height: 60.0,
+                            child: pw.Column(
+                                mainAxisAlignment:
+                                    pw.MainAxisAlignment.spaceBetween,
+                                crossAxisAlignment: pw.CrossAxisAlignment.start,
+                                children: [
+                                  pw.Text("CODIGO DE TRANSFERENCIA",
+                                      style: pw.TextStyle(
+                                          fontSize: 18.0, font: font)),
+                                  pw.Text("24352653625365532",
+                                      style: pw.TextStyle(
+                                          fontSize: 18.0, font: font)),
+                                ]),
+                          )
+                        ]),
+                  ),
                 ]),
           ); // Center
         })); // Page
